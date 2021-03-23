18b922007fbfc7bacebe99e813e2b9b48fb04fac
Nicolò Matassoli <nico.matassoli@gmail.com>
2021-03-23 18:38:26 +0100

    milestone2

--- js/script.js
+++ js/script.js
@@ -110,23 +110,48 @@ const icons = [{
   },
 ];
 
+
+const colors = [{
+    category: "food",
+    color: "yellow"
+  },
+  {
+    category: "beverage",
+    color: "blue"
+  },
+  {
+    category: "animal",
+    color: "red"
+  }
+];
+
+
+//  mappo gli elementi icons e colors, quando la category del primo è uguale a quella del secondo, trasferisco il suo colore in una nuvoa variabile colorItem, infine returno il suo valore
+const colorIcon = icons.map((icon) => {
+  let colorItem;
+  colors.forEach((item) => {
+    if (icon.category == item.category) {
+      colorItem = item.color;
+    }
+  });
+
+  icon.color = colorItem;
+  return icon;
+
+});
+
 // ciclo fra gli elementi icons, faccio destrutturazione e li ricompongo assegnandoli all'html
 icons.forEach((item) => {
   const {
     name,
     family,
-    prefix
+    prefix,
+    color
   } = item;
 
-  const html = `<div>
-<i class="${family} ${prefix}${name}"></i>
+  let html = `<div>
+<i class="${family} ${prefix}${name}" style="color: ${color}"></i>
 <div class="title">${name}</div>
 </div>`;
   $(".icons").append(html);
 });
-
-
-// Milestone 2
-// Coloriamo le icone per categoria
-// Milestone 3
-// Creiamo una select con le categorie di icone e usiamola per filtrare le icone
